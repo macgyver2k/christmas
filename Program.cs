9c986eec3e6548fad64974390a4420b133a425cf
Christopher Kloß <christopher.kloss@netscrapers.com>
2018-12-24 16:03:11 +0100

    added project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Gpio;
+
+namespace christmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");    
+
+            var blinkingPin = Pi.Gpio.Pin14;
+
+            // Configure the pin as an output
+            blinkingPin.PinMode = GpioPinDriveMode.Output;
+
+            // perform writes to the pin by toggling the isOn variable
+            var isOn = false;
+            for (var i = 0; i < 20000; i++)
+            {
+                isOn = !isOn;
+                blinkingPin.Write(isOn);
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
